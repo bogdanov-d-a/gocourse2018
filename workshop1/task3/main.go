d0f060eaf7353267b425379fe8f642c01ea5b28f
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-12-01 12:30:59 +0300

    Add squeek behavior

--- workshop1/task3/main.go
+++ workshop1/task3/main.go
@@ -22,26 +22,34 @@ func (d NoFlyBehavior) Fly() {
 	fmt.Println("Duck can't fly")
 }
 
+type QuackBehavior func()
+
 type IDuck interface {
 	Fly()
+	Quack()
 }
 
 type Duck struct {
 	fb FlyBehavior
+	qb QuackBehavior
 }
 
 func (d Duck) Fly() {
 	d.fb.Fly()
 }
 
+func (d Duck) Quack() {
+	d.qb()
+}
+
 func playWithDuck(d IDuck) {
 	d.Fly()
-	//d.Quack()
+	d.Quack()
 }
 
 func main() {
-	d1 := Duck{FlyingBehavior{}}
-	d2 := Duck{NoFlyBehavior{}}
+	d1 := Duck{FlyingBehavior{}, func() { fmt.Println("Quack") }}
+	d2 := Duck{NoFlyBehavior{}, func() { fmt.Println("Squeek") }}
 
 	playWithDuck(d1)
 	playWithDuck(d2)
