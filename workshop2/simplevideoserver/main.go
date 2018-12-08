948616c372e6b9c6f550a38acd5a5e882a5d47db
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-12-08 10:46:24 +0300

    Implement response

--- workshop2/simplevideoserver/main.go
+++ workshop2/simplevideoserver/main.go
@@ -6,8 +6,22 @@ import (
 )
 
 func main() {
-  http.HandleFunc("/hello-world", func(w http.ResponseWriter, _ *http.Request) {
-     fmt.Fprint(w,"Hello World!")
+  http.HandleFunc("/api/v1/list", func(w http.ResponseWriter, _ *http.Request) {
+    fmt.Fprint(w,`[{
+    "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
+    "name": "Black Retrospetive Woman",
+    "duration": 15,
+    "thumbnail": "/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg"
+}]`)
+  })
+  http.HandleFunc("/api/v1/video/d290f1ee-6c54-4b01-90e6-d701748f0851", func(w http.ResponseWriter, _ *http.Request) {
+    fmt.Fprint(w,`{
+    "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
+    "name": "Black Retrospetive Woman",
+    "duration": 15,
+    "thumbnail":"/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg",
+    "url":"/content/d290f1ee-6c54-4b01-90e6-d701748f0851/index.mp4" 
+}`)
   })
   http.ListenAndServe(":8000", nil)
 }
