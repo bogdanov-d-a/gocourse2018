dd913f6e81789851eef6aa11c7b12053cfe03aa1
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-12-08 12:27:18 +0300

    Add graceful shutdown

--- workshop2/simplevideoserver/main.go
+++ workshop2/simplevideoserver/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"github.com/bogdanov-d-a/gocourse2018/workshop2/simplevideoserver/handlers"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -12,11 +15,38 @@ func main() {
 	file, err := os.OpenFile("my.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
 	if err == nil {
 		log.SetOutput(file)
-		defer file.Close()
 	}
 
 	serverUrl := ":8000"
 	log.WithFields(log.Fields{"url": serverUrl}).Info("starting the server")
 	router := handlers.Router()
-	log.Fatal(http.ListenAndServe(":8000", router))
+
+	killSignalChan := getKillSignalChan()
+	srv := startServer(serverUrl, router)
+	waitForKillSignal(killSignalChan)
+	srv.Shutdown(context.Background())
+}
+
+func startServer(serverUrl string, handler http.Handler) *http.Server {
+	srv := &http.Server{Addr: serverUrl, Handler: handler}
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
+	return srv
+}
+
+func getKillSignalChan() chan os.Signal {
+	osKillSignalChan := make(chan os.Signal, 1)
+	signal.Notify(osKillSignalChan, os.Interrupt, syscall.SIGTERM)
+	return osKillSignalChan
+}
+
+func waitForKillSignal(killSignalChan <-chan os.Signal) {
+	killSignal := <-killSignalChan
+	switch killSignal {
+	case os.Interrupt:
+		log.Info("got SIGINT...")
+	case syscall.SIGTERM:
+		log.Info("got SIGTERM...")
+	}
 }
