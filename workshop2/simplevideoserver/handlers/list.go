c21f4a99520cbbf92d761cd2a9f6cf19d3c1edc0
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-12-08 12:44:46 +0300

    Use marshal in list

--- workshop2/simplevideoserver/handlers/list.go
+++ workshop2/simplevideoserver/handlers/list.go
@@ -1,15 +1,29 @@
 package handlers
 
 import (
-	"fmt"
+	"encoding/json"
+	"io"
 	"net/http"
 )
 
+type ListData struct {
+	Id        string `json:"id"`
+	Name      string `json:"name"`
+	Duration  int    `json:"duration"`
+	Thumbnail string `json:"thumbnail"`
+}
+
 func List(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprint(w, `[{
-    "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-    "name": "Black Retrospetive Woman",
-    "duration": 15,
-    "thumbnail": "/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg"
-}]`)
+	data := ListData{
+		"d290f1ee-6c54-4b01-90e6-d701748f0851",
+		"Black Retrospetive Woman",
+		15,
+		"/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg"}
+
+	dataList := [1]ListData{data}
+	b, _ := json.Marshal(dataList)
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	io.WriteString(w, string(b))
+	w.WriteHeader(http.StatusOK)
 }
