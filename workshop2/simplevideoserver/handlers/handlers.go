8b144b9c4c635ef955e1db4b41050059c7693cc2
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-12-08 11:44:34 +0300

    Add request logging

--- workshop2/simplevideoserver/handlers/handlers.go
+++ workshop2/simplevideoserver/handlers/handlers.go
@@ -2,13 +2,26 @@ package handlers
 
 import (
 	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
-func Router() *mux.Router {
+func logMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.WithFields(log.Fields{
+			"method":     r.Method,
+			"url":        r.URL,
+			"remoteAddr": r.RemoteAddr,
+			"userAgent":  r.UserAgent(),
+		}).Info("got a new request")
+		h.ServeHTTP(w, r)
+	})
+}
+
+func Router() http.Handler {
 	r := mux.NewRouter()
 	s := r.PathPrefix("/api/v1").Subrouter()
 	s.HandleFunc("/list", List).Methods(http.MethodGet)
 	s.HandleFunc("/video/d290f1ee-6c54-4b01-90e6-d701748f0851", Video).Methods(http.MethodGet)
-	return r
+	return logMiddleware(r)
 }
