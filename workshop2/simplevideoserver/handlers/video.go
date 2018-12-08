ea5a4e7cef326220838a0e852738292d318f01a2
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-12-08 12:48:34 +0300

    Use marshal in video

--- workshop2/simplevideoserver/handlers/video.go
+++ workshop2/simplevideoserver/handlers/video.go
@@ -1,16 +1,30 @@
 package handlers
 
 import (
-	"fmt"
+	"encoding/json"
+	"io"
 	"net/http"
 )
 
+type VideoData struct {
+	Id        string `json:"id"`
+	Name      string `json:"name"`
+	Duration  int    `json:"duration"`
+	Thumbnail string `json:"thumbnail"`
+	Url       string `json:"url"`
+}
+
 func Video(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprint(w, `{
-    "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-    "name": "Black Retrospetive Woman",
-    "duration": 15,
-    "thumbnail":"/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg",
-    "url":"/content/d290f1ee-6c54-4b01-90e6-d701748f0851/index.mp4" 
-}`)
+	data := VideoData{
+		"d290f1ee-6c54-4b01-90e6-d701748f0851",
+		"Black Retrospetive Woman",
+		15,
+		"/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg",
+		"/content/d290f1ee-6c54-4b01-90e6-d701748f0851/index.mp4"}
+
+	b, _ := json.Marshal(data)
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	io.WriteString(w, string(b))
+	w.WriteHeader(http.StatusOK)
 }
