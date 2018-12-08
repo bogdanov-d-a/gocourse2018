194b8b954c875c04d2ef4ccb5c35eb5bc418d480
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-12-08 22:08:16 +0300

    Extract video list data

--- workshop2/simplevideoserver/handlers/list.go
+++ workshop2/simplevideoserver/handlers/list.go
@@ -7,12 +7,6 @@ import (
 	"net/http"
 )
 
-type listDataMin struct {
-	id       string
-	name     string
-	duration int
-}
-
 type ListData struct {
 	Id        string `json:"id"`
 	Name      string `json:"name"`
@@ -21,12 +15,7 @@ type ListData struct {
 }
 
 func List(w http.ResponseWriter, _ *http.Request) {
-	ids := [3]listDataMin{
-		{"d290f1ee-6c54-4b01-90e6-d701748f0851", "Black Retrospetive Woman", 15},
-		{"sldjfl34-dfgj-523k-jk34-5jk3j45klj34", "Go Rally TEASER-HD", 41},
-		{"hjkhhjk3-23j4-j45k-erkj-kj3k4jl2k345", "Танцор", 92},
-	}
-
+	ids := GetVideoList()
 	var responseData [3]ListData
 	for i := 0; i < len(ids); i++ {
 		data_src := ids[i]
---
+++ workshop2/simplevideoserver/handlers/videolist.go
@@ -0,0 +1,15 @@
+package handlers
+
+type VideoListData struct {
+	id       string
+	name     string
+	duration int
+}
+
+func GetVideoList() [3]VideoListData {
+	return [3]VideoListData{
+		{"d290f1ee-6c54-4b01-90e6-d701748f0851", "Black Retrospetive Woman", 15},
+		{"sldjfl34-dfgj-523k-jk34-5jk3j45klj34", "Go Rally TEASER-HD", 41},
+		{"hjkhhjk3-23j4-j45k-erkj-kj3k4jl2k345", "Танцор", 92},
+	}
+}
