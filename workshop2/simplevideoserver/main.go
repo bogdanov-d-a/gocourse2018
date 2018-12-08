b421672e628929a8386e7e9a2ec9eebbecb9c3fe
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-12-08 11:37:29 +0300

    Import logger

--- workshop2/simplevideoserver/main.go
+++ workshop2/simplevideoserver/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"github.com/bogdanov-d-a/gocourse2018/workshop2/simplevideoserver/handlers"
+	log "github.com/sirupsen/logrus"
 	"net/http"
+	"os"
 )
 
 func main() {
+	log.SetFormatter(&log.JSONFormatter{})
+	file, err := os.OpenFile("my.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err == nil {
+		log.SetOutput(file)
+		defer file.Close()
+	}
+
+	serverUrl := ":8000"
+	log.WithFields(log.Fields{"url": serverUrl}).Info("starting the server")
 	router := handlers.Router()
-	fmt.Println(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
