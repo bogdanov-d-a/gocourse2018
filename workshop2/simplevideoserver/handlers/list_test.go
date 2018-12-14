286c131c0d57652a47bb76b04ef152c3e89b20e1
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2018-12-14 21:53:00 +0300

    Add list test

---
+++ workshop2/simplevideoserver/handlers/list_test.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestList(t *testing.T) {
+	w := httptest.NewRecorder()
+	List(w, nil)
+	response := w.Result()
+	if response.StatusCode != http.StatusOK {
+		t.Errorf("Status code is wrong. Have: %d, want: %d.", response.StatusCode, http.StatusOK)
+	}
+
+	jsonString, err := ioutil.ReadAll(response.Body)
+	response.Body.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+	items := make([]ListData, 10)
+	if err = json.Unmarshal(jsonString, &items); err != nil {
+		t.Errorf("Can't parse json response with error %v", err)
+	}
+}
